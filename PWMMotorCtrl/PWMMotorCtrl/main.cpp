a5d83ade4ad1524352c454d6777eba93ff1ad031
Valeriy <vaskozlov79@gmail.com>
2020-05-09 22:51:29 +0300

    Добавление поддержки АЦП.
    Добавление поддержки АЦП, для возможности управлять оборотами при помощи переменного резистора

--- PWMMotorCtrl/PWMMotorCtrl/main.cpp
+++ PWMMotorCtrl/PWMMotorCtrl/main.cpp
@@ -11,28 +11,42 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/wdt.h>
-#define BtnDwn		PINB1
-#define BtnUp		PINB2
-#define MotPWM		OCR0A
+#define BtnDwn		PINB1 // RPM Down
+#define BtnUp		PINB2 // RPM Up
+#define Jp1			PINB4 //jumper to ADC Enabled on board
+#define MotPWM		OCR0A 
 
 void portInit(void)
 {
 	DDRB = 0b00000001;
-	PORTB =0b00000110;
+	PORTB =0b00010110;
+}
+
+void adcInit(void)
+{
+	ADCSRA = 0b11000111; //Enabled, StartConversion, F_CPU/128  
+	ADMUX  = 0b01100011; // AREF-internal, ADLAR-Right, input-ADC3
 }
 
 void pwmInit(void)
 {
 	TCCR0A = 0b10000011; //FastPWM non invert OCR0A enabled
-	TCCR0B = 0b00000011;	// PWM 9600000/256/64
+	TCCR0B = 0b00000011; //PWM frequency  F_CPU/256/64
 	TCNT0 = 255;
 	OCR0A = 0;
 }
 
+uint8_t adcRead(void)
+{
+	uint8_t adc = 0;
+	ADCSRA |= (1 << ADSC);
+	adc = ADCH;
+	return adc;
+} 
 void wdt_On(void)
 {
 	wdt_reset();
-	WDTCR |= (1<<WDCE) | (1<<WDE);
+	WDTCR |= (1<<WDCE) | (1<<WDE); //WDT Change Enabled, WDT enabled
 	WDTCR = (1<<WDE) | (1<<WDP2) | (1<<WDP1); // WDT prescaler (time-out) 1s
 }
 
@@ -51,12 +65,13 @@ int main(void)
 	portInit();
 	pwmInit();
 	wdt_On();
-   
-    while (1) 
+    adcInit();
+	while (1) 
     {
+		if (~PINB & (1<<Jp1)) Motlvl = adcRead(); 
 		if (~PINB & (1<<BtnDwn)) Motlvl--;
 		if (~PINB & (1<<BtnUp)) Motlvl++;
-		if (Motlvl>=255) Motlvl = 254;
+		if (Motlvl>=254) Motlvl = 255;
 		if (Motlvl <=0) Motlvl = 1;
 		MotPWM = Motlvl;
 		wdt_reset();
